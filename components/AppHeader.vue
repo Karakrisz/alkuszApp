eef3036e4f7e4e841a28b89d077602e2dad9dd1a
Boaz <webevolution13@gmail.com>
2024-03-21 17:15:14 +0100

    upDate
    front end

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,49 +1,24 @@
-<!-- components/Header.vue -->
 <template>
-    <!-- <header>
-        <nav>
-            <NuxtLink to="/kapcsolat">kapcsolat</NuxtLink>
-        </nav>
-    </header> -->
     <header class="header">
         <div class="header-content d-flex">
-            <div class="logo-box  header-content__logo-box">
+            <div class="logo-box header-content__logo-box">
                 <a href="/" class="header-content__logo-box__link">
-                    <img class="header-content__logo-box__link__img" src="/img/header/logo.svg" alt="Biztos Alkuszom">
+                    <img src="/img/header/logo.svg" alt="Biztos Alkuszom" class="header-content__logo-box__link__img">
                 </a>
             </div>
             <div class="nav-box header-content__nav-box">
-                <div class="hamburger position-relative" id="hamburger">
-                    <div class="bar"></div>
-                    <div class="bar"></div>
-                    <div class="bar"></div>
-                </div>
                 <nav class="menu">
-                    <ul class="menu__list d-flex" id="menu__list">
-                        <li class="menu__list__li">
-                            <a class="menu__item menu__item--border-left text-color text-transform-uppercase"
-                                href="#">Főoldal</a>
-                        </li>
-                        <li class="menu__list__li">
-                            <a class="menu__item text-color text-transform-uppercase" href="#">Partnerek</a>
-                        </li>
-                        <li class="menu__list__li">
-                            <a class="menu__item text-color text-transform-uppercase" href="#">Szolgáltatások</a>
-                        </li>
-                        <li class="menu__list__li">
-                            <a class="menu__item text-color text-transform-uppercase" href="#">Szolgáltatások</a>
-                        </li>
-                        <li class="menu__list__li">
-                            <a class="menu__item text-color text-transform-uppercase" href="#">Ajánlatkérés</a>
-                        </li>
-                        <li class="menu__list__li">
-                            <a class="menu__item text-color text-transform-uppercase" href="#">Kapcsolat</a>
-                        </li>
-                        <li class="menu__list__li menu__list__li-facebook-link">
-                            <a class="menu__item text-color text-transform-uppercase" href="#">
-                                <img class="menu__list__li-facebook-link__img" src="/img/header/facebook.svg"
-                                    alt="Biztos Alkuszom">
-                            </a>
+                    <ul id="menu__list" class="menu__list d-flex">
+                        <li v-for="link in links" :key="link.path" class="menu__list__li">
+                            <NuxtLink :to="link.path"
+                                :class="['menu__item text-color text-transform-uppercase', { 'menu__item--border-left': link.path === '/' }]">
+                                <template v-if="link.imagePath">
+                                    <img :src="link.imagePath" :alt="link.name">
+                                </template>
+                                <template v-else>
+                                    {{ link.name }}
+                                </template>
+                            </NuxtLink>
                         </li>
                     </ul>
                 </nav>
@@ -51,21 +26,41 @@
         </div>
         <div class="header-bottom d-flex">
             <div class="header-bottom__div">
-                <h1 class="header-bottom__div__h1 text-color-w text-transform-uppercase">ÁGOSTON GÁBOR - <span
-                        class="header-bottom__div__span">Az Ön
-                        alkusza</span>
+                <h1 class="header-bottom__div__h1 text-color-w text-transform-uppercase">
+                    ÁGOSTON GÁBOR -
+                    <span class="header-bottom__div__span">Az Ön alkusza</span>
                 </h1>
             </div>
             <div class="header-bottom__div header-bottom__div-linkbox">
-                <a class="header-bottom__div-linkbox__link text-color-w" href="tel:+36302345866">
-                    <img class="header-bottom__div-linkbox__link__img position-relative" src="/img/header/mobile.svg"
-                        alt="Biztos Alkuszom">06 30 234 5866
+                <a href="tel:+36302345866" class="header-bottom__div-linkbox__link text-color-w">
+                    <img src="/img/header/mobile.svg" alt="Biztos Alkuszom"
+                        class="header-bottom__div-linkbox__link__img position-relative">
+                    06 30 234 5866
                 </a>
-                <a class="header-bottom__div-linkbox__link text-color-w" href="mailto:biztos@alkuszom.info">
-                    <img class="header-bottom__div-linkbox__link__img header-bottom__div-linkbox__link__img--margin-left position-relative"
-                        src="/img/header/mail.svg" alt="Biztos Alkuszom">biztos@alkuszom.info
+                <a href="mailto:biztos@alkuszom.info" class="header-bottom__div-linkbox__link text-color-w">
+                    <img src="/img/header/mail.svg" alt="Biztos Alkuszom"
+                        class="header-bottom__div-linkbox__link__img header-bottom__div-linkbox__link__img--margin-left position-relative">
+                    biztos@alkuszom.info
                 </a>
             </div>
         </div>
     </header>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            links: [
+                { name: 'Főoldal', path: '/' },
+                { name: 'Partnerek', path: '/partnerek' },
+                { name: 'Szolgáltatások', path: '/szolgaltatasok' },
+                { name: 'Dokumentumok', path: '/dokumentumok' },
+                { name: 'Ajánlatkérés', path: '/ajanlatkeres' },
+                { name: 'Kapcsolat', path: '/kapcsolat' },
+                { name: 'Facebook', path: '/logo', imagePath: '/img/header/facebook.svg' },
+            ],
+        };
+    },
+};
+</script>
