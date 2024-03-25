3ec2bf3276e239b99d1ddcf6dafd0f25c876906c
Boaz <webevolution13@gmail.com>
2024-03-25 13:05:53 +0100

    upDate
    front end

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -5,12 +5,15 @@
             <div class="footer-content__form bg-color-w">
                 <h5 class="text-transform-uppercase text-center">VEGYÜK FEL A KAPCSOLATOT!</h5>
             </div>
-            <div class="footer-content__link-box">
+            <div class="footer-content__link-box text-center">
                 <img src="/img/footer/logo.svg" alt="Biztos Alkuszom" class="footer-content__link-box__img">
                 <div class="footer-content__link-box__links">
                     <NuxtLink v-for="link in footerLinks" :key="link.path" :to="link.path" class="footer-link">
                         {{ link.name }}
                     </NuxtLink>
+                </div>
+                <div class="footer-content__link-box__DigitalSeed bg-color-w">
+                    <NuxtLink class="footer-content__link-box__DigitalSeed__Nuxtlink text-transform-uppercase" to="/">DIGITAL SEED STUDIO - 2024</NuxtLink>
                 </div>
             </div>
         </div>
