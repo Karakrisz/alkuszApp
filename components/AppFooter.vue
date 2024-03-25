2f18aa2b72d4f99388198a90863a2f265805bd01
Boaz <webevolution13@gmail.com>
2024-03-25 12:18:24 +0100

    upDate
    front end

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -5,6 +5,27 @@
             <div class="footer-content__form bg-color-w">
                 <h5 class="text-transform-uppercase text-center">VEGYÜK FEL A KAPCSOLATOT!</h5>
             </div>
+            <div class="footer-content__link-box">
+                <img src="/img/footer/logo.svg" alt="Biztos Alkuszom" class="footer-content__link-box__img">
+                <div class="footer-content__link-box__links">
+                    <NuxtLink v-for="link in footerLinks" :key="link.path" :to="link.path" class="footer-link">
+                        {{ link.name }}
+                    </NuxtLink>
+                </div>
+            </div>
         </div>
     </footer>
 </template>
+
+
+<script>
+import { Footerlinks } from '~/utils/js/links';
+
+export default {
+    data() {
+        return {
+            footerLinks: Footerlinks,
+        };
+    },
+};
+</script>
--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -48,18 +48,13 @@
 </template>
 
 <script>
+import { links } from '~/utils/js/links';
+
+
 export default {
     data() {
         return {
-            links: [
-                { name: 'Főoldal', path: '/' },
-                { name: 'Partnerek', path: '/partnerek' },
-                { name: 'Szolgáltatások', path: '/szolgaltatasok' },
-                { name: 'Dokumentumok', path: '/dokumentumok' },
-                { name: 'Ajánlatkérés', path: '/ajanlatkeres' },
-                { name: 'Kapcsolat', path: '/kapcsolat' },
-                { name: 'Facebook', path: '/logo', imagePath: '/img/header/facebook.svg' },
-            ],
+            links,
         };
     },
 };
